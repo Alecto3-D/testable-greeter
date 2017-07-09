740156c186ee09b8718afcfb5dc52e41826fc405
Kevin Griffin <papa.legba404@gmail.com>
2017-07-09 15:34:43 -0700

    intial examples to work from

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# helloworld.py
+
+from nameko.rpc import rpc
+
+class GreetingService:
+    name = "greeting_service"
+
+    @rpc
+    def hello(self, name):
+        return "Hello, {}!".format(name)
